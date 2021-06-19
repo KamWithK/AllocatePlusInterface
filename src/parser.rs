748fd71aed88a496b47e4b3af0439b89d61889ae
Kamron Bhavnagri <kamwithk@tuta.io>
2021-06-19 20:55:44 +1000

    Parser refactor for clarity and future flexibility

--- src/parser.rs
+++ src/parser.rs
@@ -16,55 +16,56 @@ pub struct Preference {
     pub popularity: f64
 }
 
-pub fn parse(raw_data: Value, semester: &str) -> Vec<Unit> {
-    let mut units = Vec::new();
+impl Unit {
+    pub fn parse_unit(unit: &str, values: Value) -> Unit {
+        // Get groups
+        let parse_groups = |(group, values): (&String, &Value)| Group::parse_group(group, values.to_owned());
+        let groups: Vec<Group> = values.get("groups").unwrap().as_object().unwrap().iter().map(parse_groups).collect();
 
-    for (unit, values) in raw_data.as_object().unwrap() {
-        // Only get this semesters units
-        if values.get("semester").unwrap() != semester {
-            continue
-        }
-
-        // Create Unit
-        units.push(
-            Unit {
-                name: unit.to_string(),
-                groups: Vec::new()
-            }
-        );
-
-        for (group, values) in values.get("groups").unwrap().as_object().unwrap() {
-            // Create group
-            units.last_mut().unwrap().groups.push(
-                Group {
-                    name: group.to_string(),
-                    preferences: Vec::new()
-                }
-            );
-
-            for (_, values) in values.get("activities").unwrap().as_object().unwrap() {
-                // Avoid non-selectable units
-                if values.get("selectable").unwrap() != "available" {
-                    continue
-                }
-
-                // Parse dates and times
-                let days = values.get("activitiesDays").unwrap().as_array().unwrap();
-                let time = values.get("start_time").unwrap().as_str().unwrap();
-
-                let to_date = |value: &Value| NaiveDateTime::parse_from_str(&format!("{}-{}", value.as_str().unwrap(), time), "%d/%m/%Y-%H:%M").unwrap();
-                let days = days.iter().map(to_date).collect();
-
-                // Create preference
-                units.last_mut().unwrap().groups.last_mut().unwrap().preferences.push(
-                    Preference {
-                        days: days,
-                        popularity: 0.
-                    }
-                );
-            }
+        // Create unit
+        Unit {
+            name: unit.to_string(),
+            groups
         }
     }
+}
+
+impl Group {
+    pub fn parse_group(group: &str, values: Value) -> Group {
+        // Get preferences
+        let filter_preferences = |(_, values): &(&String, &Value)| values.get("selectable").unwrap() != "available";
+        let parse_preferences = |(_, values): (_, &Value)| Preference::parse_preference(values.to_owned());
+        let preferences: Vec<Preference> = values.get("activities").unwrap().as_object().unwrap().iter().filter(filter_preferences).map(parse_preferences).collect();
+
+        // Create group
+        Group {
+            name: group.to_string(),
+            preferences
+        }
+    }
+}
+
+impl Preference {
+    pub fn parse_preference(values: Value) -> Preference {
+        // Parse dates and times
+        let days = values.get("activitiesDays").unwrap().as_array().unwrap();
+        let time = values.get("start_time").unwrap().as_str().unwrap();
+    
+        let to_date = |value: &Value| NaiveDateTime::parse_from_str(&format!("{}-{}", value.as_str().unwrap(), time), "%d/%m/%Y-%H:%M").unwrap();
+        let days = days.iter().map(to_date).collect();
+    
+        // Create preference
+        Preference {
+            days: days,
+            popularity: 0.
+        }
+    }
+}
+
+pub fn parse(raw_data: Value, semester: &str) -> Vec<Unit> {
+    let filter_units = |(_, values): &(&String, &Value)| values.get("semester").unwrap() == semester;
+    let parse_units = |(group, values): (&String, &Value)| Unit::parse_unit(group, values.to_owned());
+    let units: Vec<Unit> = raw_data.as_object().unwrap().iter().filter(filter_units).map(parse_units).collect();
     
     units
 }
