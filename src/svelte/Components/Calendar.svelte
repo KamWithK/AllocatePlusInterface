b0a0fa38b41832d004d9f3f97031c224492456ce
jjia0038 <jjia0038@student.monash.edu>
2021-06-24 17:47:41 +1000

    Implemented colouring of tables based on number of collisions

--- src/svelte/Components/Calendar.svelte
+++ src/svelte/Components/Calendar.svelte
@@ -1,10 +1,32 @@
 <script>
     let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
     let times = ["7am","","8am", "", "9am", "", "10am", "", "11am", "", "12pm", "", "1pm", "", "2pm", "", "3pm", "", "4pm", "", "5pm", "", "6pm", "", "7pm", "", "8pm", "", 
-    "9pm", "", "10pm"]
+    "9pm", ""]
+    let collisions = [[0,0,0,0,0,0,1,0,0,0,1,0,0,0,2,0,2,0,2,0,2,0,2,0,0,0,0,0,0,1],[0,0,2,0,0,0,2,0,1,0,1,0,0,0,2,0,1,0,2,0,1,0,1,0,0,0,0,0,0,0],[0,0,1,0,0,0,1,0,0,0,3,0,0,0,0,0,1,0,3,0,2,0,4,0,0,0,0,0,0,0],[0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,2,0,1,0,1,0,1,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+
+    function buildTable(collisions){
+        var table = document.getElementById("myTable")
+        var noRows = table.rows.length
+        var noCols = table.rows[0].cells.length
+
+        for (var x= 1; x < noCols; x++) {
+            for (var y = 1; y < noRows; y++){
+                table.rows[y].cells[x].style.backgroundColor = 'rgba(' + 15 + ',' + 255 + ',' + 255 + ',' + generateOpacity(collisions[x-1][y-1]) + ')';
+            }
+        }
+    }
+
+    function generateOpacity(collisionCell){
+        var opacity = 0;
+        let collisionCellRange = [...Array(collisionCell).keys()]
+        for (const _ in collisionCellRange){
+            opacity += 0.2
+        }
+        return opacity;
+    }
 </script>
 
-<table>
+<table id = "myTable">
     <!-- Heading for days -->
     <tr>
         <!-- Empty cell for times column -->
@@ -13,8 +35,7 @@
             <th>{day}</th>
         {/each}
     </tr>
-
-    <!-- Heading for times -->
+        <!-- Heading for times -->
     {#each times as time}
         <tr>
             <th class = "times">{time}</th>
@@ -24,6 +45,8 @@
         </tr>
     {/each}
 </table>
+
+<button on:click={() => buildTable(collisions)}>Build Table</button>
 
 <style>
     table{
